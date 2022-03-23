0fb5914b3429c09c670282b5a043e982935098a6
roshanneupane01 <roshanneupane01@gmail.com>
2022-03-22 23:51:02 -0500

    project_javaFX

---
+++ src/sample/Main.java
@@ -0,0 +1,27 @@
+/*
+Author: Roshan Neupane
+ */
+
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Mav Tunes");
+        primaryStage.setScene(new Scene(root, 800, 450));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
